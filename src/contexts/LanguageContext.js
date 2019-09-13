ad43a15882c9919339045319c3bee8cf1c523f21
FSDonnelly <donnelly_frank@yahoo.com>
2019-09-12 19:45:55 -0700

    refactor Field and Button using LanguageContext

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,2 +1,24 @@
+import React, { Component } from 'react';
 import { createContext } from 'react';
-export default createContext('english');
+
+const Context = createContext('english');
+
+export class LanguageStore extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <Context.Provider
+        value={{ ...this.state, onLanguageChange: this.onLanguageChange }}
+      >
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default Context;
