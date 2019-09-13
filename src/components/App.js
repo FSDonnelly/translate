c23cb3d9e8db5f61b03c536a738f9fc3b4d9e365
FSDonnelly <donnelly_frank@yahoo.com>
2019-09-12 19:22:49 -0700

    add LanguageSelector component to handle onLanguageChange;

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import UserCreate from './UserCreate';
+import LanguageSelector from './LanguageSelector';
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 
@@ -16,17 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className='ui container'>
-        <div>
-          Select a language:
-          <i
-            className='flag us'
-            onClick={() => this.onLanguageChange('english')}
-          />
-          <i
-            className='flag nl'
-            onClick={() => this.onLanguageChange('dutch')}
-          />
-        </div>
+        <LanguageSelector onLanguageChange={this.onLanguageChange} />
         <ColorContext.Provider value='red'>
           <LanguageContext.Provider value={this.state.language}>
             <UserCreate />
